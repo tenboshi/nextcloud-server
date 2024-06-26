usgtexty/server

feat(contactsinteraction): allow users to disable contacts interaction addressbook
This allows simple users to opt-out of the contacts interaction
addressbook even if admins have the app installed.

Similar to how the birthday calendar works, the functionnality can be
toggled in the user's settings or by doing a DELETE on the addressbook.

A new contacts personal section has been added to contain this new
setting.

Signed-off-by: Thomas Citharel <tcit@tcit.fr>

apps/contactsinteraction/composer/composer/autoload_classmap.php
@@ -12,9 +12,12 @@ return array(
     'OCA\\ContactsInteraction\\AppInfo\\Application' => $baseDir . '/../lib/AppInfo/Application.php',
     'OCA\\ContactsInteraction\\BackgroundJob\\CleanupJob' => $baseDir . '/../lib/BackgroundJob/CleanupJob.php',
     'OCA\\ContactsInteraction\\Card' => $baseDir . '/../lib/Card.php',
+    'OCA\\ContactsInteraction\\Controller\\ConfigController' => $baseDir . '/../lib/Controller/ConfigController.php',
+    'OCA\\ContactsInteraction\\DAV\\Plugin' => $baseDir . '/../lib/DAV/Plugin.php',
     'OCA\\ContactsInteraction\\Db\\CardSearchDao' => $baseDir . '/../lib/Db/CardSearchDao.php',
     'OCA\\ContactsInteraction\\Db\\RecentContact' => $baseDir . '/../lib/Db/RecentContact.php',
     'OCA\\ContactsInteraction\\Db\\RecentContactMapper' => $baseDir . '/../lib/Db/RecentContactMapper.php',
     'OCA\\ContactsInteraction\\Listeners\\ContactInteractionListener' => $baseDir . '/../lib/Listeners/ContactInteractionListener.php',
     'OCA\\ContactsInteraction\\Migration\\Version010000Date20200304152605' => $baseDir . '/../lib/Migration/Version010000Date20200304152605.php',
+    'OCA\\ContactsInteraction\\Settings\\Personal' => $baseDir . '/../lib/Settings/Personal.php',
 );

apps/contactsinteraction/composer/composer/autoload_static.php
@@ -27,11 +27,14 @@ class ComposerStaticInitContactsInteraction
         'OCA\\ContactsInteraction\\AppInfo\\Application' => __DIR__ . '/..' . '/../lib/AppInfo/Application.php',
         'OCA\\ContactsInteraction\\BackgroundJob\\CleanupJob' => __DIR__ . '/..' . '/../lib/BackgroundJob/CleanupJob.php',
         'OCA\\ContactsInteraction\\Card' => __DIR__ . '/..' . '/../lib/Card.php',
+        'OCA\\ContactsInteraction\\Controller\\ConfigController' => __DIR__ . '/..' . '/../lib/Controller/ConfigController.php',
+        'OCA\\ContactsInteraction\\DAV\\Plugin' => __DIR__ . '/..' . '/../lib/DAV/Plugin.php',
         'OCA\\ContactsInteraction\\Db\\CardSearchDao' => __DIR__ . '/..' . '/../lib/Db/CardSearchDao.php',
         'OCA\\ContactsInteraction\\Db\\RecentContact' => __DIR__ . '/..' . '/../lib/Db/RecentContact.php',
         'OCA\\ContactsInteraction\\Db\\RecentContactMapper' => __DIR__ . '/..' . '/../lib/Db/RecentContactMapper.php',
         'OCA\\ContactsInteraction\\Listeners\\ContactInteractionListener' => __DIR__ . '/..' . '/../lib/Listeners/ContactInteractionListener.php',
         'OCA\\ContactsInteraction\\Migration\\Version010000Date20200304152605' => __DIR__ . '/..' . '/../lib/Migration/Version010000Date20200304152605.php',
+        'OCA\\ContactsInteraction\\Settings\\Personal' => __DIR__ . '/..' . '/../lib/Settings/Personal.php',
     );
 
     public static function getInitializer(ClassLoader $loader)

apps/contactsinteraction/lib/DAV/Plugin.php
@@ -1,0 +1,110 @@
+<?php
+/**
+ * @copyright 2023, Thomas Citharel <nextcloud@tcit.fr>
+ *
+ * @author Thomas Citharel <nextcloud@tcit.fr>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+namespace OCA\ContactsInteraction\DAV;
+
+use OCA\ContactsInteraction\AddressBook;
+use OCA\ContactsInteraction\AppInfo\Application;
+use OCP\IConfig;
+use Sabre\DAV\Server;
+use Sabre\DAV\ServerPlugin;
+use Sabre\HTTP\RequestInterface;
+use Sabre\HTTP\ResponseInterface;
+
+/**
+ * Allows users to disable the feature by deleting the addressbook
+ *
+ * @package OCA\DAV\CalDAV\BirthdayCalendar
+ */
+class Plugin extends ServerPlugin {
+
+	protected IConfig $config;
+	protected Server $server;
+
+	public function __construct(IConfig $config) {
+		$this->config = $config;
+	}
+
+	/**
+	 * This method should return a list of server-features.
+	 *
+	 * This is for example 'versioning' and is added to the DAV: header
+	 * in an OPTIONS response.
+	 *
+	 * @return string[]
+	 */
+	public function getFeatures() {
+		return ['nc-disable-recently-contacted'];
+	}
+
+	/**
+	 * Returns a plugin name.
+	 *
+	 * Using this name other plugins will be able to access other plugins
+	 * using Sabre\DAV\Server::getPlugin
+	 *
+	 * @return string
+	 */
+	public function getPluginName() {
+		return 'nc-disable-recently-contacted';
+	}
+
+	/**
+	 * This initializes the plugin.
+	 *
+	 * This function is called by Sabre\DAV\Server, after
+	 * addPlugin is called.
+	 *
+	 * This method should set up the required event subscriptions.
+	 *
+	 * @param Server $server
+	 */
+	public function initialize(Server $server) {
+		$this->server = $server;
+
+		$this->server->on('method:DELETE', [$this, 'httpDelete']);
+	}
+
+	/**
+	 * We intercept this to handle POST requests on calendar homes.
+	 *
+	 * @param RequestInterface $request
+	 * @param ResponseInterface $response
+	 *
+	 * @return bool|void
+	 */
+	public function httpDelete(RequestInterface $request, ResponseInterface $response) {
+		$node = $this->server->tree->getNodeForPath($this->server->getRequestUri());
+		if (!($node instanceof AddressBook)) {
+			return;
+		}
+
+		$principalUri = $node->getOwner();
+		$userId = substr($principalUri, 17);
+
+		$this->config->setUserValue($userId, Application::APP_ID, 'generateContactsInteraction', 'no');
+
+		$this->server->httpResponse->setStatus(204);
+
+		return false;
+	}
+}

apps/contactsinteraction/lib/Settings/Personal.php
@@ -1,0 +1,52 @@
+<?php
+/**
+ * @copyright Copyright (c) 2023 Thomas Citharel <nextcloud@tcit.fr>
+ *
+ * @author Thomas Citharel <nextcloud@tcit.fr>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\ContactsInteraction\Settings;
+
+use OCA\ContactsInteraction\AppInfo\Application;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Services\IInitialState;
+use OCP\IConfig;
+use OCP\Settings\ISettings;
+use OCP\Util;
+
+class Personal implements ISettings {
+	public function __construct(private IInitialState $initialState, private IConfig $config, private ?string $userId) {	}
+
+	public function getForm(): TemplateResponse {
+		$this->initialState->provideInitialState('generateContactsInteraction', $this->config->getUserValue($this->userId, Application::APP_ID, 'generateContactsInteraction', 'yes') === 'yes');
+		Util::addScript(Application::APP_ID, 'settings-personal');
+		return new TemplateResponse(Application::APP_ID, 'personal');
+	}
+
+	public function getSection(): string {
+		return 'contacts';
+	}
+
+	/**
+	 * @psalm-return 40
+	 */
+	public function getPriority(): int {
+		return 40;
+	}
+}

tenboshi/nextcloud-server

apps/contactsinteraction/composer/composer/autoload_classmap.php
@@ -12,9 +12,12 @@ return array(
     'OCA\\ContactsInteraction\\AppInfo\\Application' => $baseDir . '/../lib/AppInfo/Application.php',
     'OCA\\ContactsInteraction\\BackgroundJob\\CleanupJob' => $baseDir . '/../lib/BackgroundJob/CleanupJob.php',
     'OCA\\ContactsInteraction\\Card' => $baseDir . '/../lib/Card.php',
+    'OCA\\ContactsInteraction\\DAV\\Plugin' => $baseDir . '/../lib/DAV/Plugin.php',
     'OCA\\ContactsInteraction\\Db\\CardSearchDao' => $baseDir . '/../lib/Db/CardSearchDao.php',
     'OCA\\ContactsInteraction\\Db\\RecentContact' => $baseDir . '/../lib/Db/RecentContact.php',
     'OCA\\ContactsInteraction\\Db\\RecentContactMapper' => $baseDir . '/../lib/Db/RecentContactMapper.php',
     'OCA\\ContactsInteraction\\Listeners\\ContactInteractionListener' => $baseDir . '/../lib/Listeners/ContactInteractionListener.php',
+    'OCA\\ContactsInteraction\\Listeners\\UserPreferenceListener' => $baseDir . '/../lib/Listeners/UserPreferenceListener.php',
     'OCA\\ContactsInteraction\\Migration\\Version010000Date20200304152605' => $baseDir . '/../lib/Migration/Version010000Date20200304152605.php',
+    'OCA\\ContactsInteraction\\Settings\\Personal' => $baseDir . '/../lib/Settings/Personal.php',
 );

apps/contactsinteraction/composer/composer/autoload_static.php
@@ -27,11 +27,14 @@ class ComposerStaticInitContactsInteraction
         'OCA\\ContactsInteraction\\AppInfo\\Application' => __DIR__ . '/..' . '/../lib/AppInfo/Application.php',
         'OCA\\ContactsInteraction\\BackgroundJob\\CleanupJob' => __DIR__ . '/..' . '/../lib/BackgroundJob/CleanupJob.php',
         'OCA\\ContactsInteraction\\Card' => __DIR__ . '/..' . '/../lib/Card.php',
+        'OCA\\ContactsInteraction\\DAV\\Plugin' => __DIR__ . '/..' . '/../lib/DAV/Plugin.php',
         'OCA\\ContactsInteraction\\Db\\CardSearchDao' => __DIR__ . '/..' . '/../lib/Db/CardSearchDao.php',
         'OCA\\ContactsInteraction\\Db\\RecentContact' => __DIR__ . '/..' . '/../lib/Db/RecentContact.php',
         'OCA\\ContactsInteraction\\Db\\RecentContactMapper' => __DIR__ . '/..' . '/../lib/Db/RecentContactMapper.php',
         'OCA\\ContactsInteraction\\Listeners\\ContactInteractionListener' => __DIR__ . '/..' . '/../lib/Listeners/ContactInteractionListener.php',
+        'OCA\\ContactsInteraction\\Listeners\\UserPreferenceListener' => __DIR__ . '/..' . '/../lib/Listeners/UserPreferenceListener.php',
         'OCA\\ContactsInteraction\\Migration\\Version010000Date20200304152605' => __DIR__ . '/..' . '/../lib/Migration/Version010000Date20200304152605.php',
+        'OCA\\ContactsInteraction\\Settings\\Personal' => __DIR__ . '/..' . '/../lib/Settings/Personal.php',
     );
 
     public static function getInitializer(ClassLoader $loader)

apps/contactsinteraction/lib/DAV/Plugin.php
@@ -1,0 +1,110 @@
+<?php
+/**
+ * @copyright 2023, Thomas Citharel <nextcloud@tcit.fr>
+ *
+ * @author Thomas Citharel <nextcloud@tcit.fr>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+namespace OCA\ContactsInteraction\DAV;
+
+use OCA\ContactsInteraction\AddressBook;
+use OCA\ContactsInteraction\AppInfo\Application;
+use OCA\ContactsInteraction\Db\RecentContactMapper;
+use OCP\IConfig;
+use Sabre\DAV\Server;
+use Sabre\DAV\ServerPlugin;
+use Sabre\HTTP\RequestInterface;
+use Sabre\HTTP\ResponseInterface;
+
+/**
+ * Allows users to disable the feature by deleting the addressbook
+ *
+ * @package OCA\DAV\CalDAV\BirthdayCalendar
+ */
+class Plugin extends ServerPlugin {
+
+	protected Server $server;
+
+	public function __construct(protected IConfig $config, protected RecentContactMapper $recentContactMapper) {
+	}
+
+	/**
+	 * This method should return a list of server-features.
+	 *
+	 * This is for example 'versioning' and is added to the DAV: header
+	 * in an OPTIONS response.
+	 *
+	 * @return string[]
+	 */
+	public function getFeatures() {
+		return ['nc-disable-recently-contacted'];
+	}
+
+	/**
+	 * Returns a plugin name.
+	 *
+	 * Using this name other plugins will be able to access other plugins
+	 * using Sabre\DAV\Server::getPlugin
+	 *
+	 * @return string
+	 */
+	public function getPluginName() {
+		return 'nc-disable-recently-contacted';
+	}
+
+	/**
+	 * This initializes the plugin.
+	 *
+	 * This function is called by Sabre\DAV\Server, after
+	 * addPlugin is called.
+	 *
+	 * This method should set up the required event subscriptions.
+	 *
+	 * @param Server $server
+	 */
+	public function initialize(Server $server) {
+		$this->server = $server;
+
+		$this->server->on('method:DELETE', [$this, 'httpDelete']);
+	}
+
+	/**
+	 * We intercept this to handle POST requests on contacts homes.
+	 *
+	 * @param RequestInterface $request
+	 * @param ResponseInterface $response
+	 *
+	 * @return bool|void
+	 */
+	public function httpDelete(RequestInterface $request, ResponseInterface $response) {
+		$node = $this->server->tree->getNodeForPath($this->server->getRequestUri());
+		if (!($node instanceof AddressBook)) {
+			return;
+		}
+
+		$principalUri = $node->getOwner();
+		$userId = substr($principalUri, 17);
+
+		$this->config->setUserValue($userId, Application::APP_ID, 'disableContactsInteractionAddressBook', 'yes');
+		$this->recentContactMapper->cleanForUser($userId);
+
+		$this->server->httpResponse->setStatus(204);
+
+		return false;
+	}
+}

apps/contactsinteraction/lib/Settings/Personal.php
@@ -1,0 +1,52 @@
+<?php
+/**
+ * @copyright Copyright (c) 2023 Thomas Citharel <nextcloud@tcit.fr>
+ *
+ * @author Thomas Citharel <nextcloud@tcit.fr>
+ *
+ * @license GNU AGPL version 3 or any later version
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ *
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ *
+ */
+
+namespace OCA\ContactsInteraction\Settings;
+
+use OCA\ContactsInteraction\AppInfo\Application;
+use OCP\AppFramework\Http\TemplateResponse;
+use OCP\AppFramework\Services\IInitialState;
+use OCP\IConfig;
+use OCP\Settings\ISettings;
+use OCP\Util;
+
+class Personal implements ISettings {
+	public function __construct(private IInitialState $initialState, private IConfig $config, private ?string $userId) {	}
+
+	public function getForm(): TemplateResponse {
+		$this->initialState->provideInitialState('generateContactsInteraction', $this->config->getUserValue($this->userId, Application::APP_ID, 'disableContactsInteractionAddressBook', 'no') === 'no');
+		Util::addScript(Application::APP_ID, 'settings-personal');
+		return new TemplateResponse(Application::APP_ID, 'personal');
+	}
+
+	public function getSection(): string {
+		return 'contacts';
+	}
+
+	/**
+	 * @psalm-return 40
+	 */
+	public function getPriority(): int {
+		return 40;
+	}
+}